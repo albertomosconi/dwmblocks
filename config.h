149644e4fe34c7c0d10fe0582ed1a4510a3f59d0
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-07-11 19:25:08 +0200

    new scripts

--- config.h
+++ config.h
@@ -1,11 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"volume",	0,	10},
-	{"",	"internet",	5,	4},
-	{"",	"battery",	5,	3},
-	{"",	"clock",	60,	1},
+    //{"",    "news",     300,      6},
+    {"",    "weather",  18000,  5},
+	{"",	"volume",	0,	    10},
+	{"",	"internet",	5,	    4},
+    {"",    "disk",     10,     2},
+	{"",	"battery",	5,	    3},
+	{"",	"clock",	15,	    1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char *delim = " ";
+static char *delim = "  ";
